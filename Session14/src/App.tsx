48beef5beafe39d53ae35b5512f22850f463d91e
Pham Viet An <anphamls6969@gmail.com>
2025-08-24 23:03:31 +0700

    ss14-2

--- Session14/src/App.tsx
+++ Session14/src/App.tsx
@@ -1,23 +1,23 @@
-// import Exercise01 from "./components/Exersice01"
-// import Notification from "./components/Notification"
-// import Ex03 from "./components/Ex03"
-// import Ex04 from "./components/Ex04"
-// import Ex05 from "./components/Ex05"
-// import Ex06 from "./components/Ex06"
-// import Ex07 from "./components/Ex07"
+import Exercise01 from "./components/Exersice01"
+import Notification from "./components/Notification"
+import Ex03 from "./components/Ex03"
+import Ex04 from "./components/Ex04"
+import Ex05 from "./components/Ex05"
+import Ex06 from "./components/Ex06"
+import Ex07 from "./components/Ex07"
 import Ex08 from "./components/Ex08"
 
 function App() {
 
   return (
     <>
-      {/* <Exercise01 />
+      <Exercise01 />
       <Notification />
       <Ex03 />
       <Ex04 />
       <Ex05 />
       <Ex06 />
-      <Ex07 /> */}
+      <Ex07 />
       <Ex08 />
     </>
   )
